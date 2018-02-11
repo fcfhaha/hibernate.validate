b3ca650eb0c2c0ab08d4bfa782418466569b7ef6
fcfhaha <fcfhaha@126.com>
2018-02-11 15:55:08 +0800

    add interface CarGroup

---
+++ src/main/java/com/hibernate/validator/group/CarGroup.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.hibernate.validator.group;
+
+/**
+ * @author V.Caifeng.Fan
+ * @ClassName CarGroup
+ * @Description: TODO
+ * @date 2018年2月11日 下午3:38:47
+ */
+public interface CarGroup {
+
+}
